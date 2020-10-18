89dfff74f09409c5381d081ad58747819c6cd5d6
lisi <1786678583@qq.com>
2020-10-18 16:00:41 +0800

    Initial commit

---
+++ src/test/java/com/lisi/gitideatest/GitIdeaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisi.gitideatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
